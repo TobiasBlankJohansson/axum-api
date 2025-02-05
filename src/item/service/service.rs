2c456bf48586a86a268d4959e0b409bda47b5827
TobiasBlankJohansson <tobias@annito.se>
2025-02-05 13:32:43 +0100

    feat:add create_item in Service

--- src/item/service/service.rs
+++ src/item/service/service.rs
@@ -5,7 +5,11 @@ use crate::item::repository::repository::Repository;
 pub struct Service;
 
 impl Service {
-    pub async fn get_item_list(pool: &PgPool) -> Vec<Item>{
+    pub async fn get_item_list(pool: &PgPool) -> Vec<Item> {
         Repository::inventory_list(pool).await
+    }
+
+    pub async fn create_item(pool: &PgPool, name: &String, quantity: &i16, storage_area: &String) -> uuid {
+        Repository::create_item(pool, name, quantity, storage_area)
     }
 }
