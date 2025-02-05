df8561e69f80eb8ed866120a8365593662c06b38
TobiasBlankJohansson <tobias@annito.se>
2025-02-05 15:30:13 +0100

    feat:add get_item_by_id

--- src/item/service/service.rs
+++ src/item/service/service.rs
@@ -10,6 +10,10 @@ impl Service {
         Repository::inventory_list(pool).await
     }
 
+    pub async fn get_item(pool: &PgPool, id: Uuid) -> Item {
+        Repository::get_item_by_id(pool, id)
+    }
+
     pub async fn create_item(pool: &PgPool, name: &String, quantity: &i16, storage_area: &String) -> Uuid {
         Repository::create_item(pool, name, quantity, storage_area).await
     }
