7bc0ada8b69c98a2cd0e9df7aafe594f12783839
TobiasBlankJohansson <tobias@annito.se>
2025-02-05 16:54:44 +0100

    feat:add update_item in main

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ mod item;
 
 use std::net::SocketAddr;
 use axum::{Router};
-use axum::routing::{delete, get, post};
+use axum::routing::{delete, get, post, put};
 use dotenv::dotenv;
 use item::database::database::establish_connection;
-use crate::item::controller::controller::{create_item, delete_item, get_item, get_items};
+use crate::item::controller::controller::{create_item, delete_item, get_item, get_items, update_item};
 
 #[tokio::main]
 async fn main() {
@@ -13,13 +13,14 @@ async fn main() {
     let pool = establish_connection().await;
 
     let app = Router::new()
-        .route("/api/items",get(get_items))
+        .route("/api/items", get(get_items))
         .route("/api/items/:id", get(get_item))
-        .route("/api/items",post(create_item))
-        .route("/api/items/:id",delete(delete_item))
+        .route("/api/items", post(create_item))
+        .route("/api/items/:id", delete(delete_item))
+        .route("/api/items/:id", put(update_item))
         .with_state(pool);
 
-    let addr = SocketAddr::from(([127,0,0,1],3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
     let listener = tokio::net::TcpListener::bind(addr)
         .await
