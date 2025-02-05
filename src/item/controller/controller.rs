62595a46485773ed14c0ddebfb3ed55f472b47b2
TobiasBlankJohansson <tobias@annito.se>
2025-02-05 16:38:57 +0100

    feat:add delete_item in controller

--- src/item/controller/controller.rs
+++ src/item/controller/controller.rs
@@ -1,5 +1,7 @@
 use axum::extract::{Path, State};
+use axum::http::StatusCode;
 use axum::Json;
+use axum::response::IntoResponse;
 use serde::{Deserialize};
 use sqlx::PgPool;
 use uuid::Uuid;
@@ -28,4 +30,8 @@ pub async fn create_item(
     Json(body): Json<CreateItemRequest>) -> Json<Uuid> {
     let item_uuid = Service::create_item(&pool, &body.name, &body.quantity, &body.storage_area).await;
     Json(item_uuid)
+}
+pub async fn delete_item(State(pool): State<PgPool>, Path(id): Path<Uuid>) -> impl IntoResponse {
+    Service::delete_item(&pool, id).await;
+    StatusCode::NO_CONTENT
 }
