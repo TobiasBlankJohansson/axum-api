bbd6a4b17c908605a9fbb79dd90f1e311b558c7d
TobiasBlankJohansson <tobias@annito.se>
2025-02-05 10:40:44 +0100

    feat:add PgPool in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod item;
+
+use dotenv::dotenv;
+use item::database::database::establish_connection;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let pool = establish_connection().await;
+
 }
