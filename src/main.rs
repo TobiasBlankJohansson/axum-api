4098c201f32f7614657ed5ee326d8ea930046f62
TobiasBlankJohansson <tobias@annito.se>
2025-02-06 09:19:28 +0100

    feat:add CorsLayer to allow origin any

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,17 @@ use std::net::SocketAddr;
 use axum::{Router};
 use axum::routing::{delete, get, post, put};
 use dotenv::dotenv;
+use tower_http::cors::{Any, CorsLayer};
 use item::database::database::establish_connection;
 use crate::item::controller::controller::{create_item, delete_item, get_item, get_items, update_item};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any);
+
     let pool = establish_connection().await;
 
     let app = Router::new()
@@ -18,7 +23,8 @@ async fn main() {
         .route("/api/items", post(create_item))
         .route("/api/items/:id", delete(delete_item))
         .route("/api/items/:id", put(update_item))
-        .with_state(pool);
+        .with_state(pool)
+        .layer(cors);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
