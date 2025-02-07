0f56fb8d94a3aeb10289dd3fac3998583b147e1e
TobiasBlankJohansson <tobias@annito.se>
2025-02-07 11:16:32 +0100

    refa:remove unneeded code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 mod item;
 
 use std::net::SocketAddr;
-use std::sync::Arc;
 use dotenv::dotenv;
-use tokio::sync::Mutex;
 use tower_http::cors::{Any, CorsLayer};
 use utoipa::OpenApi;
 use utoipa_axum::router::OpenApiRouter;
-use utoipa_axum::routes;
 use utoipa_swagger_ui::SwaggerUi;
-use item::database::database::establish_connection;
-use crate::item::controller::controller::{create_item, delete_item, get_item, get_items, router, update_item, ApiDoc};
+use crate::item::controller::controller::{router, ApiDoc};
 
 #[tokio::main]
 async fn main() {
@@ -18,7 +14,6 @@ async fn main() {
 
     let cors = CorsLayer::new()
         .allow_origin(Any);
-
 
     let (router, api) = OpenApiRouter::with_openapi(ApiDoc::openapi())
         .nest("/api/v1/items", router().await)
