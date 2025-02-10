4308b7d58f79a09462d98233ba23c516c20bc9a5
TobiasBlankJohansson <Tobias@annito.se>
2025-02-10 11:23:38 +0100

    refa:refactor database.rs to be more cousemuseble

--- src/item/database/database.rs
+++ src/item/database/database.rs
@@ -2,6 +2,15 @@ use sqlx::PgPool;
 use std::env;
 
 pub async fn establish_connection() -> PgPool {
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let postgres_user = env::var("POSTGRES_USER").expect("DATABASE_URL must be set");
+    let postgres_password = env::var("POSTGRES_PASSWORD").expect("DATABASE_URL must be set");
+    let postgres_db = env::var("POSTGRES_DB").expect("DATABASE_URL must be set");
+    let postgres_host = env::var("POSTGRES_HOST").unwrap_or_else(|_| "localhost".to_string());
+    let postgres_port = env::var("POSTGRES_PORT").unwrap_or_else(|_| "5432".to_string());
+
+    let database_url = format!(
+        "postgresql://{}:{}@{}:{}/{}",
+        postgres_user, postgres_password, postgres_host, postgres_port, postgres_db
+    );
     PgPool::connect(&database_url).await.expect("Failed to connect to database")
 }
